92ca365d731e2002960d4a48efcb4dcf93ffcab7
Jan Skowronski <crianonim@gmail.com>
2023-07-21 15:36:49 +0100

    add svg

--- src/Main.elm
+++ src/Main.elm
@@ -3,6 +3,8 @@ module Main exposing (..)
 import Browser exposing (Document)
 import Html exposing (Html)
 import Html.Attributes as Attrs
+import Svg
+import Svg.Attributes as Svg
 
 
 main : Program String Model Msg
@@ -37,4 +39,17 @@ view : Model -> Html msg
 view model =
     Html.div [ Attrs.class "border border-slate-500" ]
         [ Html.h1 [] [ Html.text "Carmen Herrera" ]
+        , Html.div [ Attrs.class "border border-slate-500 w-[300px] " ]
+            [ Svg.svg
+                [ Svg.width "100%"
+                , Svg.viewBox "0 0 1111 1257"
+                , Svg.class "bg-[#f3f3f3]"
+                ]
+                [ Svg.polygon
+                    [ Svg.points "0,480 555,424 1111,480"
+                    , Svg.fill "#017f45"
+                    ]
+                    []
+                ]
+            ]
         ]
